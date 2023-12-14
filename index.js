fe5c613e9cf5abd7a9fc89d6a86378e4d7004229
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2023-12-14 20:29:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity) {
+    switch (activity) {
+        case "bathe my dog":
+            return "This Saturday, I want to bathe my dog!";
+            break;
+            
+            default:
+                return "This Saturday, I want to roller-skate!";
+                break;
+
+    }
+}
+function mondayWork(activity){
+    switch (activity) {
+        case "work from home":
+            return "This Monday, I will work from home.";
+            break;
+
+            default:
+                return "This Monday, I will go to the office.";
+                break;
+    }
+}
+function wrapAdjective(wrapper="*") {
+    
+        return function(adjective=" a hard worker"){
+            return`You are ${wrapper}${adjective}${wrapper}!`
+        }
+    }
+    
